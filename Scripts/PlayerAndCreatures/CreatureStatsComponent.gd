8d97f3ec8dc22c75b8885b15fd4519711f1d4083
Otto Elomaa <otto.elomaa99@gmail.com>
2025-01-21 14:17:11 +0200

    Started doing player stats and equipable items

---
+++ Scripts/PlayerAndCreatures/CreatureStatsComponent.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+
+@export var guard := 0
+@export var crit := 0
+@export var evade := 0
+@export var block := 0
---
+++ Scripts/equip_item.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+
+@export var topDamage := 0
+@export var midDamage := 0
+@export var lowDamage := 0
